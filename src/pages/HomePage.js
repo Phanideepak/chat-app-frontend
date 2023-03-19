b3d115dde3cc00c14d725d06dd4cca3b6529f07f
Phanideepak_laevo <phanideepak1729@gmail.com>
2023-03-19 19:02:09 +0530

    chakra ui login panel changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,37 @@
+import { Box, Container,Tab,TabList,TabPanel,TabPanels,Tabs,Text } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW="xl" centerContent>
+      <Box d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Poppins"  color="black">Test to Leave live</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px" >
+        <Tabs variant='soft-rounded' colorScheme='green'>
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign up</Tab>
+          </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
